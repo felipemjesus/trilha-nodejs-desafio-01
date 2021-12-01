8d7dc1cc88c0f9223061d8a5b67d42745bf09284
Felipe Martins <felipemjesus@outlook.com>
2021-11-30 23:18:29 -0300

    Create todo

--- src/index.js
+++ src/index.js
@@ -59,7 +59,29 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  const { title, deadline } = request.body;
+
+  if (!title) {
+    return response.status(400).json({ error: 'Title is required!' });
+  }
+
+  if (!deadline) {
+    return response.status(400).json({ error: 'Deadline is required!' });
+  }
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  };
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
